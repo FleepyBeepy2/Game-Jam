8bb3598aefaf899888dda06a378e7a997292f900
sierra foley <85092803+Subnitica@users.noreply.github.com>
2024-06-01 14:54:49 -0400

    adding actual order functionality

--- My project/Assets/Scripts/HighlightFood.cs
+++ My project/Assets/Scripts/HighlightFood.cs
@@ -4,18 +4,45 @@ using UnityEngine;
 
 public class HighlightFood : MonoBehaviour
 {
-    // Start is called before the first frame update
-    
-   private void OnMouseEnter()
-{
-    GetComponent<Renderer>().material.color = new Color(1f, 1f, 1f); // White color
-    Debug.Log("Enter collider");
-}
+    public int toppingID; // Unique ID for the topping, set this in the Inspector
+    private Renderer renderer;
 
-private void OnMouseExit()
-{
-    GetComponent<Renderer>().material.color = new Color(94f / 255f, 94f / 255f, 94f / 255f); // Original color
-    Debug.Log("Exit collider");
-}
+    // List to store the current attempt at the customer order
+    private static List<int> currentAttemptOrder = new List<int>();
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    private void OnMouseEnter()
+    {
+        renderer.material.color = new Color(1f, 1f, 1f); // White color
+        Debug.Log("Enter collider");
+    }
+
+    private void OnMouseExit()
+    {
+        renderer.material.color = new Color(94f / 255f, 94f / 255f, 94f / 255f); // Original color
+        Debug.Log("Exit collider");
+    }
+
+    private void OnMouseDown()
+    {
+        currentAttemptOrder.Add(toppingID);
+        Debug.Log("Topping added: " + toppingID);
+    }
+
+    // Optional: Method to clear the current attempt order list
+    public static void ClearCurrentAttemptOrder()
+    {
+        currentAttemptOrder.Clear();
+    }
+
+    // Optional: Method to get the current attempt order list
+    public static List<int> GetCurrentAttemptOrder()
+    {
+        return new List<int>(currentAttemptOrder); // Return a copy of the list
+    }
 
 }
