7f09578a740dfc342252000ddff0197cf6c820af
FleepyBeepy <pcg3290@gmail.com>
2024-06-01 12:17:24 -0500

    Added Meat

---
+++ My project/Assets/Scripts/CookMeat.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookMeat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float rawMeat = 0.0f;
+    public float cookedMeat = 5.0f;
+    public float overCookedMeat = 10.0f;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter() 
+    {
+       
+    }
+}
